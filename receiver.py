3b9103f3879a0b41271caaf97024667b37191282
romai <sokolovgit256@gmail.com>
2023-05-23 01:06:42 +0600

    fastApi commit

--- receiver.py
+++ receiver.py
@@ -1,9 +1,31 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import wave
+import requests
+import json
 
-# Открываем звуковой файл для чтения
-with wave.open('Sound.wav', 'r') as file:
-    # Получаем параметры звукового файла
-    channels, _, framerate, _, _, _ = file.getparams()
+app2 = FastAPI()
+app2.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app2.post("/receiver")
+async def convert_to_txt(data: str):
+
+    response = requests.get(data)
+    with open('Sound.wav', 'wb') as f:
+        f.write(response.content)
+
+    # Открываем звуковой файл для чтения
+    with wave.open('Sound.wav', 'r') as file:
+        # Получаем параметры звукового файла
+        channels, _, framerate, _, _, _ = file.getparams()
 
     # Читаем данные из звукового файла
     binary = ''
@@ -19,7 +41,12 @@ with wave.open('Sound.wav', 'r') as file:
     for i in range(0, len(binary), 8):
         text += chr(int(binary[i:i+8], 2))
 
-# Записываем текст в новый файл
-with open('NewFile.txt', 'w', encoding='utf-8') as file:
-    file.write(text)
+    # Записываем текст в новый файл
+    with open('NewFile.txt', 'w', encoding='utf-8') as file:
+        file.write(text)
 
+    # Возвращаем содержимое файла в качестве ответа
+    with open('NewFile.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    return {"content": content}
--- transmitte.py
+++ transmitte.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import wave
 
-app = FastAPI()
 
-@app.post("/convert")
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/transmitte")
 async def convert_to_wav(file: UploadFile = File(...)):
     # Открываем файл и считываем его содержимое
     contents = await file.read()
@@ -13,7 +25,7 @@ async def convert_to_wav(file: UploadFile = File(...)):
     binary = ''.join(format(byte, '08b') for byte in contents)
 
     # Создаем новый wave файл
-    with wave.open('temp.wav', 'w') as f:
+    with wave.open('Sound.wav', 'w') as f:
         # Устанавливаем параметры звукового файла
         f.setparams((1, 1, 44100, 0, 'NONE', 'not compressed'))
 
@@ -25,4 +37,4 @@ async def convert_to_wav(file: UploadFile = File(...)):
                 f.writeframes(bytes([255]))
 
     # Возвращаем wav файл в качестве ответа на запрос
-    return FileResponse('temp.wav', media_type='audio/wav')
+    return FileResponse(path='Sound.wav', filename='123', media_type='audio/wav')
